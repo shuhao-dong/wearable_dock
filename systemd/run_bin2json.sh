4f154988f41061144a3adccbfc9a76de6f919b52
Shuhao Dong <shuhao.dong@bristol.ac.uk>
2025-07-01 16:14:47 +0100

    Integrate to a single wearable dock service

--- systemd/run_bin2json.sh
+++
@@ -1,9 +0,0 @@
-sudo tee /usr/local/bin/run_bin2json.sh > /dev/null << 'EOF'
-#!/usr/bin/env bash
-
-sleep 3
-
-exec /usr/local/bin/bin2json_mqtt
-EOF
-
-sudo chmod +x /usr/local/bin/run_bin2json.sh
